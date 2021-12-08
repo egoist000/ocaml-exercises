24437936b32d9547251b6095dcad3bcea1bfb1b5
Lorenzo <lor.epifani@stud.uniroma3.it>
2021-12-08 17:45:20 +0100

    completato esercizi gruppo 4

--- 04/exercises/2.ml
+++ 04/exercises/2.ml
@@ -64,4 +64,79 @@ let enumera lst =
         | x::rest -> (i, x)::aux (i + 1) rest
     in aux 0 lst
 
+(* position: a' -> a' list -> int *)
+(* position x lst = la posizione della prima occorrenza di x in lst.
+ * se l'elemento non è presente solleva un eccezione *)
 
+let position x lst =
+    let rec loop i = function
+        | [] -> raise Not_found
+        | e::rest -> 
+                if x = e then i
+                else loop (i + 1) rest
+    in loop 0 lst
+
+let rec position' x = function
+    | [] -> raise Not_found
+    | e::rest -> 
+            if x = e then 1
+            else 1 + position' x rest
+
+(* alternate: a' list -> a' list *)
+(* alternate lst = una lista che riporta gli elementi di lst 
+ * con indice dispari *)
+
+let alternate lst = 
+    let rec loop i = function
+        | [] -> []
+        | x::rest -> 
+                if i mod 2 <> 0 then x::loop (i + 1) rest
+                else loop (i + 1) rest
+    in loop 0 lst
+
+exception Err
+
+let maxlst lst =
+    let rec aux m = function
+        | [] -> m
+        | x::rest -> aux (max m x) rest
+    in try aux (List.hd lst) lst with
+    | _ -> raise Err
+
+let rec min_dei_max = function
+    | [] -> raise Err
+    | [lst] -> maxlst lst
+    | lst::rest -> min (maxlst lst) (min_dei_max rest)
+
+let min_dei_max' lst =
+    let rec loop mlst = function
+        | [] -> mlst
+        | x::rest -> loop (min mlst (maxlst x)) rest
+    in 
+    match lst with
+    | [] -> raise Err
+    | x::rest -> loop (maxlst x) rest
+
+let rec drop n = function
+    | [] -> []
+    | x::rest -> 
+            if n <= 0 then x::rest
+            else drop (n - 1) rest;;
+
+let rec take n = function
+    | [] -> []
+    | x::rest -> 
+            if n <= 0 then []
+            else x::take (n - 1) rest;;
+
+let split2 lst =
+    let rec aux l = function
+        | [] -> l
+        | x::rest -> aux (l + 1) rest
+    in
+    match lst with
+    | [] -> ([], [])
+    | [x] -> ([x], [])
+    | lista ->
+            let len = aux 0 lista
+            in (take (len / 2) lista, drop (len / 2) lista)
